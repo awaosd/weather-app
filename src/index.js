0654860fb2990c98648ead1fd2a1e0e520dea0a3
awaosd <osunadiaz.eva@gmail.com>
2025-08-21 19:14:08 +0100

    Added function and api for weather

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
+function findWeather(response) {
+  let currentTemperature = document.querySelector("#current-temperature");
+  currentTemperature.innerHTML = `${Math.round(
+    response.data.temperature.current
+  )}`;
+  let city = document.querySelector("#forecast-city");
+  city.innerHTML = `${response.data.city}`;
+}
+
+function findCity(city) {
+  let apiKey = "3t7b46o8f5d3a094fe4a456f3ea46695";
+  let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(url).then(findWeather);
+}
+
 function searchCity(event) {
   event.preventDefault();
   let formCity = document.querySelector("#search-city");
-  let city = document.querySelector("#forecast-city");
-  city.innerHTML = `${formCity.value}`;
+
+  findCity(formCity.value);
 }
 
 let SearchForm = document.querySelector("#search-form");
 SearchForm.addEventListener("submit", searchCity);
+
+findCity("London");
