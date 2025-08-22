0dff60687cdb50dab5361da70bcce80f57c1456e
awaosd <osunadiaz.eva@gmail.com>
2025-08-22 11:04:05 +0100

    added more weather data from api

--- src/index.js
+++ src/index.js
@@ -5,6 +5,34 @@ function findWeather(response) {
   )}`;
   let city = document.querySelector("#forecast-city");
   city.innerHTML = `${response.data.city}`;
+  let details = document.querySelector("#details");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let date = new Date(response.data.time * 1000);
+
+  details.innerHTML = `${response.data.condition.description}`;
+  humidity.innerHTML = `${response.data.temperature.humidity}%`;
+  wind.innerHTML = `${response.data.wind.speed}km/h`;
+  dateElement.innerHTML = formatDate(date);
+}
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
 
 function findCity(city) {
